d411fa1026eb89c1f4ecdcb4f45286d42f7c4b33
bxbach732 <buixuanbach2000@gmail.com>
2022-11-10 00:17:52 +0200

    Added authentication to mongodb container

--- src/db/database.js
+++ src/db/database.js
@@ -1,9 +1,16 @@
+require("dotenv").config({ path: ".env" });
 const mongoose = require("mongoose");
 
 try {
     mongoose.connect('mongodb://mongodb:27017/virtual-bar', {
         useNewUrlParser: true, 
-        useUnifiedTopology: true 
+        useUnifiedTopology: true,
+        auth: {
+            "username": process.env.MONGO_INITDB_ROOT_USERNAME,
+            "password": process.env.MONGO_INITDB_ROOT_PASSWORD
+        },
+        authSource: "admin",
+        authMechanism: "DEFAULT"
     });
     console.log('Database connected ...');
 } catch (err) {    
