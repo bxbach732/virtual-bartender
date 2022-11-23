720c455c615f529c4b8ec4ce8c672c840ae15cea
Giang Le <giang.le@fliq.fi>
2022-11-23 02:59:28 +0200

    styling for RecipeList page

--- client/src/components/materialui/styles.js
+++ client/src/components/materialui/styles.js
@@ -3,10 +3,48 @@ import { makeStyles } from "@mui/styles";
 //The styles for the app are done here
 //Import and call the useStyles function to create a classes object, holding all of the css.
 const useStyles = makeStyles((theme) => ({
-  example: {
-    width: "35%",
-    color: red,
+  gridContainer: {
+    //backgroundColor: "green",
+    justifyContent: "center",
+    alignItems: "center",
+
+    '&>.MuiGrid-item':{
+      paddingLeft:'5rem !important'
+    },
+
+    '& .link-item':{
+      minWidth:'10rem'
+    }
   },
+
+  recipesContainer:{
+    width:'48rem',
+    margin:'0 auto 5rem',
+    border:'1px solid #333'
+  },
+
+  recipe:{
+    padding:'1rem',
+
+    '&:first-child':{
+      paddingLeft:0
+    },
+
+    '&:last-child':{
+      paddingRight:0
+    },
+
+    '&>.img':{
+      width:'90%',
+
+      '&>img':{
+        //By default, it should be 100%
+        maxWidth:'100%'
+      }
+    }
+
+  }
+
 }));
 
 export default useStyles;
